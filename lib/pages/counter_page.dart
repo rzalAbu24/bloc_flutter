35944be9047d96b2e08b6f93d18c4fd033d61036
rzalAbu24 <rizalabu024@gmail.com>
2024-05-04 20:27:18 +0800

    Testing bloc

---
+++ lib/pages/counter_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_flutter/bloc/counter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+        backgroundColor: Colors.green,
+      ),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) => Center(
+            child: Text(
+          '$count',
+          style: const TextStyle(fontSize: 50),
+        )),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => context.read<CounterCubit>().increment(),
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
